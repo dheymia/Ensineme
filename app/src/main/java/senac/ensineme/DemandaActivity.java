986c9f6ec4ee6898d2b5d4c4614fe2b2f1a5136d
wendelabreu <wendel6236@gmail.com>
2019-08-19 23:45:35 -0300

    Altera DemandaActivity

--- app/src/main/java/senac/ensineme/DemandaActivity.java
+++ app/src/main/java/senac/ensineme/DemandaActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
@@ -26,14 +27,26 @@ import senac.ensineme.models.Demanda;
 
 public class DemandaActivity extends ComumActivity implements DatabaseReference.CompletionListener, View.OnClickListener {
 
-    private Button btnCadastrar;
-    private Context context;
-    private EditText txtLocalDemanda, txtDescDemanda;
-    private CalendarView inicioDemanda;
-    private Spinner spnCatDemanda, spnTurnoDemanda, spnValidadeDemanda, spnHorasaulaDemanda;
-    private String aluno, codDemanda, catDemanda, descDemanda, horasaulaDemanda, turnoDemanda, localDemanda;
-    private Date diaSelecionado, dataDemanda;
-    Demanda demanda;
+    //Não serão entradas do usuário no cadastro a demanda
+    private String alunoDemanda; //obtida a partir do login e vinculada a demanda cadastrada
+    private String codDemanda; //obtida a partir do firebase
+    private Date dataDemanda; //obtida do sistema ou firebase na data do cadastro da demanda
+    private Date expiraDemanda; //obtida da soma da data da demanda aos dias de validade da demanda
+
+    //Entradas do usuário no cadastro a demanda
+    private Button btnCadastrar; //enviará os dados para o firebase
+    private AutoCompleteTextView txtDescDemanda; // descrição suscinta da demanda
+    private EditText txtLocalDemanda; //selecionado através da API viaCEP
+    private CalendarView inicioDemanda; //selecionado a partir de uma calendário
+    private Spinner spnCatDemanda; // Dança, Esporte, Instrumentos, Culinária e Disciplinas
+    private Spinner spnTurnoDemanda; // matutino, verpertino e nortuno
+    private Spinner spnValidadeDemanda; // Em dias: de 1 a 30 ou multiplos de 2 até 30 ou multiplos de 5 até 30
+    private Spinner spnHorasaulaDemanda; //Em horas-aula (50 minutos): de 1 a 30 ou multiplos de 2 até 30 ou multiplos de 5 até 30
+    private Demanda demanda;
+
+    // Armazena entradas
+    private String descricao, categoria, turno, horasaula, local;
+    private Date datadeinicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
